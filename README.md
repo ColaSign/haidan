6c0dac811b4b4f7c573435adee5030fe07820df5
jokin <327928971@qq.com>
2020-07-16 16:08:11 +0800

    Ver. 0.0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+# 海胆之家签到器
+
+## 使用方法
+
+- **Fork** 本项目
+- 登录[海胆之家](https://www.haidan.video/)后，获得Cookies
+  - `c_secure_uid`
+  - `c_secure_pass`
+  - `*c_secure_login`(可选/非必要)
+- 设置`Secrets`
+  - `HAIDAN_UID`：获取的`c_secure_uid`
+  - `HAIDAN_PASS`：获取的`c_secure_pass`
+  - `*HAIDAN_LOGIN`：获取的`c_secure_login`(可选/非必要)
+- 任意修改`README.md`以启动`Action`
---
+++ main.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import urllib3
+
+def sign() :
+    r = HTTP.request('GET', SIGNURL, headers=HEADERS)
+    if (r.status != 200) :
+        print('!! 签到失败，错误的状态： ' + str(r.status))
+        exit(1)
+    # 获取最新魔力值
+    print('-> 尝试更新魔力值')
+    r = HTTP.request('GET', BASEURL, headers=HEADERS)
+    if (r.status != 200) :
+        print('!! 错误的状态： ' + str(r.status))
+        exit(1)
+    data = r.data.decode('utf-8')
+    pattern = re.compile('<span\s*id=[\'|"]magic_num[\'|"]>([0-9,\.]+)\([\s\S]+\)</span>')
+    mn = re.search(pattern, data)
+    if mn:
+        mn = float(mn.group(1).replace(',', ''))
+        d = mn - MAGIC_NUM
+        print('-> 签到成功，获得魔力值：' + str(d))
+    else:
+        print('!! 获取最新魔力值失败')
+        exit(3)
+
+def get_status() :
+    global MAGIC_NUM
+
+    print('-> 正在获取用户状态...')
+    r = HTTP.request('GET', BASEURL, headers=HEADERS)
+    if (r.status != 200) :
+        print('!! 错误的状态： ' + str(r.status))
+        exit(1)
+    data = r.data.decode('utf-8')
+
+    # 用户名
+    pattern = re.compile('<a\s*href=[\'|"][\s\S]+[\'|"]\s*class=[\'|"]User_Name[\'|"]\s*>\s*<b>\s*(.+)</b>\s*</a>')
+    username = re.search(pattern, data)
+    if username:
+        print('-> 当前用户：' + username.group(1))
+    else:
+        print('-> 登录身份过期或程序失效')
+        exit(2)
+
+    # 魔力值
+    pattern = re.compile('<span\s*id=[\'|"]magic_num[\'|"]>([0-9,\.]+)\([\s\S]+\)</span>')
+    mn = re.search(pattern, data)
+    if mn:
+        MAGIC_NUM = float(mn.group(1).replace(',', ''))
+        print('-> 当前魔力值：' + str(MAGIC_NUM))
+    else:
+        print('-> 登录身份过期或程序失效')
+        exit(2)
+
+    # 打卡状态
+    pattern = re.compile('<input\s*type=[\'|"]submit[\'|"]\s*id=[\'|"]modalBtn[\'|"]\s*class=[\'|"]dt_button[\'|"]\s*value=[\'|"]已经打卡[\'|"][\s]*/[\s]*>')
+    signed = re.search(pattern, data)
+    if not signed:
+        sign()  #签到
+    else:
+        print('-> 今日已签到')
+
+def main() :
+    print("=================================================")
+    print("||                 HaiDan Sign                 ||")
+    print("||                Author: Jokin                ||")
+    print("||               Version: v0.0.1               ||")
+    print("=================================================")
+
+    global BASEURL
+    BASEURL = 'https://www.haidan.video/index.php'
+
+    global SIGNURL
+    SIGNURL = 'https://www.haidan.video/signin.php'
+
+    global HTTP
+    HTTP = urllib3.PoolManager()
+
+    _uid = os.getenv('HAIDAN_UID') if os.getenv('HAIDAN_UID') else False
+    _pass = os.getenv('HAIDAN_PASS') if os.getenv('HAIDAN_PASS') else False
+    _login = os.getenv('HAIDAN_LOGIN') if os.getenv('HAIDAN_LOGIN') else 'bm9wZQ%3D%3D'
+
+    if not _uid or not _pass or not _login:
+        print('!! 缺少设置： 环境变量/Secrets')
+        exit(4)
+
+
+    global HEADERS
+    HEADERS = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        'cookie': 'c_secure_login=' + _login + '; c_secure_uid=' + _uid + '; c_secure_pass=' + _pass,
+    }
+
+    global MAGIC_NUM
+    MAGIC_NUM = 0
+
+    get_status()
+
+if __name__ == '__main__':
+    main()
